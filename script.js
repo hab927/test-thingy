1edcb215c57dc0779cceb8b04a8a5841e6331434
hab 927 <warmairballoon@gmail.com>
2023-11-13 01:44:11 -0800

    upgrade button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var count = 0;
+var addAmount = 1;
 
 function increase() {
-    count++;
+    count += addAmount;
     document.getElementById("count").getElementsByClassName("fancy")[0].innerHTML = count;
+}
+
+function upgrade() {
+    addAmount += 1;
+    document.getElementsByClassName("upgradeParam")[0].innerHTML = addAmount;
 }
